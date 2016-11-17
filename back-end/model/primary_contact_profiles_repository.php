94188d6a31dca06b90991af4b5a4bd93314db547
hanbomb713 <hanbomb@hotmail.com>
2016-11-17 10:39:57 -0800

    Update primary_contact_profiles_repository.php

--- back-end/model/primary_contact_profiles_repository.php
+++ back-end/model/primary_contact_profiles_repository.php
@@ -1,9 +1,9 @@
 <?php
 
 /*  
- * Student Info: Name=Arvin Mai, ID=10010
+ * Student Info: Name=Han Wu, ID=16327
  * Subject: NPU_2016_Fall_CS556(A)_Team_Project
- * Author: Arvin-tcm 
+ * Author: Han 
  * Filename: primary_contact_profiles_repository.php 
  * Date and Time: Nov 10, 2016 11:18:38 PM 
  * Project Name: CS556_Team_Project 
